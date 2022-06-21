961e4d99d30a40e8a07f481c4af4f1c8d250e7a3
Jéssica Martins <jeemmartins@gmail.com>
2022-06-21 10:27:09 -0300

    iniciando api

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const urls = new Map();
+
+let index = 0;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  index += 1;
+
+  urls.set(index, req.body.url);
+
+  res.status(200).json({
+    url: `http://localhost:3000/${index}`,
+  });
 });
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  const url = urls.get(+id);
+
+  if (!url) {
+    res.status(404).json({ message: "Not found" });
+    return;
+  }
+
+  res.redirect(url);
+});
+
 app.listen(3000, () => console.log("http://localhost:3000"));
